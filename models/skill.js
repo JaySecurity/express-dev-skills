c1f7e0cca5ee64c18bc3c98c51dfee6f8e049eb7
Jason Nicholls <jasonnicholls1981@gmail.com>
2021-04-20 17:15:40 -0400

    modify getOne function to use filter method

--- models/skill.js
+++ models/skill.js
@@ -1,21 +1,21 @@
 const skills = [
-  {id: 0, name: "HTML", level: "Intermediate"},
-  {id: 1, name: "CSS", level: "Intermediate"},
-  {id: 2, name: "Javascript", level: "Intermediate"},
-  {id: 3, name: "Python", level: "Novice"},
-  {id: 4, name: "React", level: "Beginner"},
+  { id: 0, name: 'HTML', level: 'Intermediate' },
+  { id: 1, name: 'CSS', level: 'Intermediate' },
+  { id: 2, name: 'Javascript', level: 'Intermediate' },
+  { id: 3, name: 'Python', level: 'Novice' },
+  { id: 4, name: 'React', level: 'Beginner' },
+];
 
-]
-
-function getAll(){
-  return skills
+function getAll() {
+  return skills;
 }
 
-function getOne(id){
-  return skills[id]
+function getOne(id) {
+  const skill = skills.filter((skill) => skill.id === parseInt(id));
+  return skill[0];
 }
 
 module.exports = {
   getAll,
   getOne,
-}
+};
